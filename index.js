eafff11c1d0c28cf942f37642eae6d53e1836106
Bobs <bobis.daniel.bscs2023@gmail.com>
2024-05-25 20:59:58 +0800

    BUILDING 2 DONE

--- index.js
+++ index.js
@@ -279,25 +279,41 @@ function searchButton(building, floor) {
                 hideAllFloors();
                 building2floor2.style.visibility = 'visible';
             }
-                informationheader.textContent = "Management Information System Office";
-                informationdetails.innerHTML = "<br>The Management Information System Office is responsible in handling the university's information system. <br><br> The office is responsible in managing the university's AIMS Portal and ensuring that all data are organized and correct. <br><br> Office Hours: <br><br>Monday - Friday <br> 8:00 AM - 5:00 PM";
-                fblink.textContent = "UCC MIS - North ";
-                weblink.textContent = "MIS NORTH ";
-                logoimg.src = 'misnorthlogo.jpg';
-                fafacebok.href = 'https://www.facebook.com/uccmisnorth';
-                fblink.href = 'https://www.facebook.com/uccmisnorth';
+                informationheader.textContent = "Department of Tourism and Hospitality Industry Management";
+                informationdetails.innerHTML = "<br><br>The Department of Tourism and Hospitality Industry Management offers a practical education in tourism and hospitality, covering customer service, event management, hotel operations, and marketing. Through partnerships with industry players, students gain real-world experience for careers in these fields.";
+                fblink.textContent = "no information available ";
+                weblink.textContent = "---";
+                logoimg.src = 'claslogo.png';
+                fafacebok.href = '';
+                fblink.href = '';
             
         } else if (floor === 3) {
             document.getElementById('floor32').style.backgroundColor = 'red';
             if (building2floor3) {
                 hideAllFloors();
                 building2floor3.style.visibility = 'visible';
+
+                informationheader.textContent = "Computer Studies Department";
+                informationdetails.innerHTML = "<br><br>This floor is managed by Allicance of E-Computer Students(ACES) <br><br>educate students in various aspects of computer science and technology, including programming, software engineering, cybersecurity, artificial intelligence, and data science.";
+                fblink.textContent = "CSDNorthUCC";
+                weblink.textContent = "---";
+                logoimg.src = 'csdlogo.jpg';
+                fafacebok.href = 'https://www.facebook.com/CSDNorthUCC';
+                fblink.href = 'https://www.facebook.com/CSDNorthUCC';
             }
         } else if (floor === 4) {
             document.getElementById('floor42').style.backgroundColor = 'red';
             if (building2floor4) {
                 hideAllFloors();
                 building2floor4.style.visibility = 'visible';
+
+                informationheader.textContent = "Bachelor of Public Administration Department";
+                informationdetails.innerHTML = "<br>This floor is managed by the Bachelor of Public Administration Department.<br><br>The Bachelor of Public Administration Department teaches students about public sector management, policies, and governance. It equips them with skills in leadership, policy analysis, and organizational management for careers in government and related fields.";
+                fblink.textContent = "ucclingkodbayan";
+                weblink.textContent = "---";
+                logoimg.src = 'bpalogo.png';
+                fafacebok.href = 'https://www.facebook.com/ucclingkodbayan';
+                fblink.href = 'https://www.facebook.com/ucclingkodbayan';
             }
         } else if (floor === 5) {
             document.getElementById('social').style.backgroundColor = 'red';
@@ -391,6 +407,90 @@ function show103() {
             informationheader.textContent = "Room 103 & 104";
             informationdetails.innerHTML = "This room is used by the following programs: <br><br>Bachelor of Secondary Education Major in English <br><br> Bachelor of Secondary Education Major in English-Chinese <br><br> Bachelor in Secondary Education Major in Technology and Livelihood Education.  ";
 
+            informationheader.classList.add('fade-in');
+            informationdetails.classList.add('fade-in');
+        }, 500); // The timeout should be the same as the transition duration in the CSS
+    }
+}
+
+function showCS() {
+    if (informationheader) {
+        // Add the 'fade' class to start the fade out effect
+        informationheader.classList.add('fade');
+        informationdetails.classList.add('fade');
+
+        // After the fade out effect is complete, change the text and start the fade in effect
+        setTimeout(function() {
+            informationheader.textContent = "BS in Computer Science";
+            informationdetails.innerHTML = "This room is managed by Computer Science council. <br><br> Bachelor of Science in Computer Science (BSCS) centers on the theoretical foundations and principles of computing, covering topics such as algorithms, programming languages, software development, and computational theory.";
+            fblink.textContent = "CS Council";
+            weblink.textContent = "---";
+            logoimg.src = 'cslogo.png';
+            fafacebok.href = 'https://www.facebook.com/UCCBSCS2022';
+            fblink.href = 'https://www.facebook.com/UCCBSCS2022';
+            informationheader.classList.add('fade-in');
+            informationdetails.classList.add('fade-in');
+        }, 500); // The timeout should be the same as the transition duration in the CSS
+    }
+}
+
+function showIS() {
+    if (informationheader) {
+        // Add the 'fade' class to start the fade out effect
+        informationheader.classList.add('fade');
+        informationdetails.classList.add('fade');
+
+        // After the fade out effect is complete, change the text and start the fade in effect
+        setTimeout(function() {
+            informationheader.textContent = "BS in Information System";
+            informationdetails.innerHTML = "This room is managed by Information System council. <br><br> Bachelor of Science in Information Systems (BSIS) combines business and computing technology, focusing on designing and managing information systems, data analytics, and the strategic use of technology in organizations.";
+            fblink.textContent = "IS Council";
+            weblink.textContent = "---";
+            logoimg.src = 'islogo.png';
+            fafacebok.href = 'https://www.facebook.com/profile.php?id=100091523451762';
+            fblink.href = 'https://www.facebook.com/profile.php?id=100091523451762';
+            informationheader.classList.add('fade-in');
+            informationdetails.classList.add('fade-in');
+        }, 500); // The timeout should be the same as the transition duration in the CSS
+    }
+}
+
+function showIT() {
+    if (informationheader) {
+        // Add the 'fade' class to start the fade out effect
+        informationheader.classList.add('fade');
+        informationdetails.classList.add('fade');
+
+        // After the fade out effect is complete, change the text and start the fade in effect
+        setTimeout(function() {
+            informationheader.textContent = "BS in Information Technology";
+            informationdetails.innerHTML = "<br><br>This room is managed by Information Technology council. <br><br> Bachelor of Science in Information Technology (BSIT) focuses on the practical applications of technology in business and organizations, emphasizing skills in networking, database management, web development, and IT support.";
+            fblink.textContent = "IT Council";
+            weblink.textContent = "---";
+            logoimg.src = 'itlogo.png';
+            fafacebok.href = 'https://www.facebook.com/profile.php?id=100091323001194';
+            fblink.href = 'https://www.facebook.com/profile.php?id=100091323001194';
+            informationheader.classList.add('fade-in');
+            informationdetails.classList.add('fade-in');
+        }, 500); // The timeout should be the same as the transition duration in the CSS
+    }
+}
+
+function showEMC() {
+    if (informationheader) {
+        // Add the 'fade' class to start the fade out effect
+        informationheader.classList.add('fade');
+        informationdetails.classList.add('fade');
+
+        // After the fade out effect is complete, change the text and start the fade in effect
+        setTimeout(function() {
+            informationheader.textContent = "BS in Entertainment and Multimedia Computing";
+            informationdetails.innerHTML = "<br><br>This room is managed by Entertainment and Multimedia Computing council. <br><br> Bachelor of Science in Entertainment and Multimedia Computing (BSEMC) concentrates on the study of digital entertainment and multimedia technologies, including game development, multimedia production, animation, and interactive media design.";
+            fblink.textContent = "EMC Council";
+            weblink.textContent = "---";
+            logoimg.src = 'emclogo.png';
+            fafacebok.href = 'https://www.facebook.com/profile.php?id=100093705553269';
+            fblink.href = 'https://www.facebook.com/profile.php?id=100093705553269';
             informationheader.classList.add('fade-in');
             informationdetails.classList.add('fade-in');
         }, 500); // The timeout should be the same as the transition duration in the CSS
